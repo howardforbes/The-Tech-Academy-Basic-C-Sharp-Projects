0e8a4b3f18e9e3d2a67938ca11a689f375c6cede
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-11 14:13:08 -0700

    Added another line to test

--- ExerciseA/ExerciseA/Program.cs
+++ ExerciseA/ExerciseA/Program.cs
@@ -12,6 +12,7 @@ namespace ExerciseA
         {
             Console.WriteLine("Welcome to Acme Accounting Systems");
             Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.WriteLine("Here is another message");
             Console.ReadLine();
         }
     }
