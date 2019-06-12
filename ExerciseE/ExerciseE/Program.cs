3bde574050686935de8be8fa9cc71ff33e7f1837
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-12 11:44:22 -0700

    Added ExerciseE project

---
+++ ExerciseE/ExerciseE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+
+            string strAge = Console.ReadLine();
+            int intAge = Convert.ToInt32(strAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+
+            string strDUI = Console.ReadLine();
+            bool blDUI = Convert.ToBoolean(strDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+
+            string strSpeedingTickets = Console.ReadLine();
+            int intSpeedingTickets = Convert.ToInt32(strSpeedingTickets);
+
+            Console.WriteLine("Qualified?");
+
+            //COMPARISON
+
+            bool isQualified = (intAge > 15 && blDUI == false && intSpeedingTickets <= 3);
+
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+
+
+        }
+    }
+}
