72643bd30ccaf3d1bb3e1a85c207ffbe18f12d9e
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-14 15:33:48 -0700

    Added ExerciseM

---
+++ ExerciseM/ExerciseM/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseM
+{
+    public class MyClass
+    {
+        public void MethodOne(int number1, int number2)
+        {
+            number1 = number1 + 100;
+            Console.WriteLine(number2);
+        }
+    }
+}
---
+++ ExerciseM/ExerciseM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass();
+
+            myclass.MethodOne(5, 10);
+            myclass.MethodOne(number1: 5, number2: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
