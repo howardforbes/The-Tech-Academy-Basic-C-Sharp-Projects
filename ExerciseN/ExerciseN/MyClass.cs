5d3ff4854ec95dda918495b8e56fee9bddfcb507
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-14 15:59:56 -0700

    Added ExerciseN

---
+++ ExerciseN/ExerciseN/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseN
+{
+    public class MyClass
+    {
+        public int DoMultiplication(int num)
+        {
+            int result = num * 10;
+            return result;
+        }
+
+        public int DoMultiplication(decimal num)
+        {
+            int result = Convert.ToInt32(num / 2);
+            return result;
+        }
+
+        public int DoMultiplication(string num)
+        {
+            int intNum = Convert.ToInt32(num);
+            int result = intNum - 5;
+            return result;
+        }
+    }
+}
---
+++ ExerciseN/ExerciseN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClassInstance = new MyClass();
+
+            Console.WriteLine("The result of method 1 is: " + myClassInstance.DoMultiplication(12));
+            Console.WriteLine("The result of method 2 is: " + myClassInstance.DoMultiplication(12m));
+            Console.WriteLine("The result of method 2 is: " + myClassInstance.DoMultiplication("12"));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
