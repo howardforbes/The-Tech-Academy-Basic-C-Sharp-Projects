b10e966972cceff9de735fcd61a9bf4fbeea9645
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-13 17:30:24 -0700

    Added ExerciseJ project

---
+++ ExerciseJ/ExerciseJ/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// PROBLEM 1
+
+            //string[] stringArray = new string[5];
+
+            //for (int i = 0; i < stringArray.Length; i++)
+            //{
+            //    Console.WriteLine("Enter some text");
+            //    stringArray[i] = Console.ReadLine();
+            //}
+
+            //for (int i = 0; i < stringArray.Length; i++)
+            //{
+            //    Console.WriteLine(stringArray[i]);
+            //}
+
+            //// PROBLEM 2 AND 3
+
+            //bool condition = true;
+            //int count = 0;
+
+            //while (condition)
+            //{
+            //    Console.WriteLine("This is one iteration");
+            //    count = count + 1;  //added this line to make the loop stop
+            //    if (count == 10)
+            //    {
+            //        condition = false;
+            //    }
+
+            //}
+
+            //// PROBLEM 4
+
+            //int[] intArray = new int[] { 10, 20, 30, 40, 60 };
+
+            //for (int i = 0; i < intArray.Length; i++)
+            //{
+            //    Console.WriteLine(intArray[i]);
+            //}
+
+            //// PROBLEM 5
+
+            //int[] intArray2 = new int[] { 10, 20, 30, 40, 60 };
+
+            //for (int i = 0; i <= intArray2.Length - 1; i++)
+            //{
+            //    Console.WriteLine(intArray2[i]);
+            //}
+
+            // PROBLEM 6, 7, 8
+
+            //List<string> names = new List<string>() { "Howard", "Joe", "Mack", "Mark", "John" };
+
+            //Console.WriteLine("Enter text to search for string");
+            //string searchString = Console.ReadLine();
+
+            //bool isInList = false;
+            //for(int i = 0; i < names.Count; i++)
+            //{
+            //    if(searchString == names[i])
+            //    {
+            //        Console.WriteLine("The index is: " + i);
+            //        isInList = true;
+            //        break;  //stops loop from executing once a match is found
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //if(isInList == false)
+            //{
+            //    Console.WriteLine("The text was not found in the List");
+            //}
+
+            // PROBLEM 9 AND 10
+
+            //List<string> names2 = new List<string>() { "Howard", "Jim", "John", "Mark", "Mark" };
+            
+            //Console.WriteLine("Enter text to search for in the list");
+            //string searchText = Console.ReadLine();
+
+            //bool isInList2 = false;
+
+            //for(int i = 0; i < names2.Count; i++)
+            //{
+            //    if(searchText == names2[i])
+            //    {
+            //        Console.WriteLine(i);
+            //        isInList2 = true;
+            //    }
+            //}
+
+            //if(!isInList2)
+            //{
+            //    Console.WriteLine("The text is not in the List");
+            //}
+
+            // PROBLEM 11
+
+            //List<string> listOfStrings = new List<string>() { "Howard", "Jim", "John", "Mark", "Mark" };
+            //List<string> copyOfList = new List<string>();
+            
+            //Console.WriteLine("Enter a name to search for in the List");
+            //string searchText2 = Console.ReadLine();
+            
+            //foreach (string name in listOfStrings)
+            //{
+            //    if(searchText2 == name)
+            //    {
+            //        copyOfList.Add(name);
+                    
+            //        if (copyOfList.Count == 2)
+            //        {
+            //            Console.WriteLine(name + " already in the list");
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(name);
+            //    }
+                           
+            //}
+
+           
+          
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
