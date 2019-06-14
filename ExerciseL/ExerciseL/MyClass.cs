cd13bbe8cb4e1bb9b257113e86d5773925175155
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-14 14:41:56 -0700

    Added ExerciseL project

---
+++ ExerciseL/ExerciseL/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseL
+{
+    public class MyClass
+    {
+        public int MethodOne(int number)
+        {
+            int result;
+            result = number * 10;
+            return result;
+        }
+
+        public int MethodTwo(int number)
+        {
+            int result;
+            result = number / 2;
+            return result;
+        }
+
+        public int MethodThree(int number)
+        {
+            int result;
+            result = number + 100;
+            return result;
+        }
+    }
+
+}
---
+++ ExerciseL/ExerciseL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass();
+
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number multiplied by 10 is: " + myclass.MethodOne(number));
+            Console.WriteLine("The number divided by 2 is: " + myclass.MethodTwo(number));
+            Console.WriteLine("The number plus 100 is :" + myclass.MethodThree(number));
+            Console.ReadLine();
+        }
+    }
+}
