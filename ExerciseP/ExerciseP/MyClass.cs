7d4e65afd95af14067b38742a33b0092d17fd52c
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-15 15:47:21 -0700

    Added ExerciseP

---
+++ ExerciseP/ExerciseP/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP
+{
+    class MyClass
+    {
+        public static void MethodOne(int num1, out int num2)
+        {
+            int result = num1 / 2;
+            num2 = result;
+        }
+
+        public static void MethodOne(int num1, out int num2, out int num3)
+        {
+            int result1 = num1 * 5;
+            int result2 = num1 - 5;
+            num2 = result1;
+            num3 = result2;
+        }
+    }
+}
---
+++ ExerciseP/ExerciseP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = 0;
+            int num3 = 0;
+
+            MyClass.MethodOne(num1, out num2);
+
+            Console.WriteLine("The result of MethodOne: "+ num2);
+
+            MyClass.MethodOne(num1, out num2, out num3);
+
+            Console.WriteLine("The result of the overloaded Method1: " + num2 + ", " + num3);
+
+            Console.ReadLine();
+        }
+    }
+}
