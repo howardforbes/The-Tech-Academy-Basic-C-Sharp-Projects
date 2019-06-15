e67b55450870845213d67cf17173cede62f8a3a9
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-14 18:22:45 -0700

    Added ExerciseO

---
+++ ExerciseO/ExerciseO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass();
+
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number, or none at all");
+            string input = Console.ReadLine();
+
+            if (input == "")
+            {
+                Console.WriteLine("The result is: " + myclass.DoOperation(num1));
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(input);
+                Console.WriteLine("The result is: " + myclass.DoOperation(num1, num2));
+            }
+
+                Console.ReadLine();
+         }
+    }
+}
