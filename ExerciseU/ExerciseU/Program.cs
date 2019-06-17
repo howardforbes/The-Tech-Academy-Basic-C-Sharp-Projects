1c9bed27976d1812c71b5256400da13c8fe4479e
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-17 14:20:36 -0700

    Added ExerciseU

---
+++ ExerciseU/ExerciseU/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
