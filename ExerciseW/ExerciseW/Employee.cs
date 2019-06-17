59bf415b81e39cf626d8e6e0eed884f93a325e9e
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-17 16:31:42 -0700

    Added ExerciseW

---
+++ ExerciseW/ExerciseW/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseW
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            if(emp1.Id == emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            if(emp1.Id != emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ExerciseW/ExerciseW/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            Employee b = new Employee();
+
+            a.firstName = "Howard";
+            a.lastName = "Forbes";
+            a.Id = 5;
+
+            b.firstName = "Bobby";
+            b.lastName = "Jones";
+            b.Id = 10;
+
+            bool result = a == b;
+
+            Console.WriteLine("Object A == B: " + result);
+
+            bool result2 = a != b;
+
+            Console.WriteLine("Object A != B: " + result2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
