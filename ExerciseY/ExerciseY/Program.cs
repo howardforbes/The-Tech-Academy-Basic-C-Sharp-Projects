37eaa69e545007ea7797e11c779289652ab63b31
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-18 13:05:43 -0700

    Added ExerciseY

---
+++ ExerciseY/ExerciseY/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter day of the week.  Example: Monday");
+                DaysOfTheWeek value = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine("The day you entered is: " + value.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+           
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
