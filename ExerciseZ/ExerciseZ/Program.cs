c88313811d9cf71dbd054c7098938ecb1b4e005f
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-18 13:38:26 -0700

    Added ExerciseZ

---
+++ ExerciseZ/ExerciseZ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 45.50m;
+
+            Console.WriteLine("The amount is: " + number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
