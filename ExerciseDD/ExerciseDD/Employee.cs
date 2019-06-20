5418eb4dcc31b6f0c137e0d62a4cee5487091506
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-20 14:59:06 -0700

    Added ExerciseDD

---
+++ ExerciseDD/ExerciseDD/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDD
+{
+    public class Employee
+    {
+        
+        public Employee(string fname, string lname) : this(fname, lname, 10)
+        {
+
+        }
+        public Employee(string fname, string lname, int id)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Id = id;
+
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ ExerciseDD/ExerciseDD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string greeting_fname = "Please enter your first name";
+            const string greeting_lname = "Please enter your last name";
+            Console.WriteLine(greeting_fname);
+            var fname = Console.ReadLine();
+            Console.WriteLine(greeting_lname);
+            var lname = Console.ReadLine();
+
+            Employee employee = new Employee(fname, lname);
+
+            Console.WriteLine("The employee id is {0}", employee.Id);
+            Console.ReadLine();
+        }
+    }
+}
