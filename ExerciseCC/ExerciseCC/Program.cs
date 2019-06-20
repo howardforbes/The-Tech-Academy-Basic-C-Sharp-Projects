7e4b014c088b4731a7d5eb5fc43c259c1fb12b63
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-20 12:57:58 -0700

    Added ExerciseCC

---
+++ ExerciseCC/ExerciseCC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The time in " + hours + " hours will be " + DateTime.Now.AddHours(hours).ToString("h:mm:ss tt"));
+            Console.ReadLine();
+        }
+    }
+}
