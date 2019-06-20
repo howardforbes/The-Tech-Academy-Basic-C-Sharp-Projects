2ffe10bae106a12d816537b2edc725b381e6a122
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-20 12:19:14 -0700

    Added ExerciseBB

---
+++ ExerciseBB/ExerciseBB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExerciseBB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\howar\Logs\exercise.txt", number);
+            string text = File.ReadAllText(@"C:\Users\howar\Logs\exercise.txt");
+            Console.WriteLine("The contents of the text file are: " + text);
+            Console.ReadLine();
+
+        }
+    }
+}
