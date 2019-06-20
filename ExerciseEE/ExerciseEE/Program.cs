9287519db3bddd636ad16391bd5b7d3f730fe165
Howard Forbes <howard.d.forbes@outlook.com>
2019-06-20 16:30:58 -0700

    Added ExerciseEE

---
+++ ExerciseEE/ExerciseEE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter you age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if(age <= 0)
+                {
+                    throw new InvalidOperationException();
+                }
+                int year = DateTime.Now.Year;
+                int result = year - age;
+                Console.WriteLine("The year you were born is " + result);
+                Console.ReadLine();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Input less than or equal to zero not permitted!");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input!");
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
