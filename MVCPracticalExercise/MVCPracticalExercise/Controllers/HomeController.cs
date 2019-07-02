129eba5cf0c876938edcc528f62c5ff9b7592434
Howard Forbes <howard.d.forbes@outlook.com>
2019-07-02 14:42:20 -0700

    Added MVCPracticalExercise

---
+++ MVCPracticalExercise/MVCPracticalExercise/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using MVCPracticalExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPracticalExercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult Admin(string firstName, string lastName, string emailAddress, DateTime dateOfBirth,
+                                    int carYear, string carMake, string carModel, int speedingTickets,
+                                    string radioDui, string radioCoverage)
+       {
+            decimal quote = 50m;
+            int userAge = Convert.ToInt32(DateTime.Now.Year - dateOfBirth.Year);
+
+            ViewBag.Year = userAge.ToString();
+
+            if (userAge < 25)
+            {
+                quote = quote + 25m;
+            }
+
+            if (userAge < 18)
+            {
+                quote = quote + 100m;
+            }
+
+            if (userAge > 100)
+            {
+                quote = quote + 25m;
+            }
+
+            if (carYear < 2000)
+            {
+                quote = quote + 25m;
+            }
+
+            if (carYear > 2015)
+            {
+                quote = quote + 25m;
+            }
+
+            if (carMake.ToLower() == "porsche")
+            {
+                quote = quote + 25m;
+            }
+
+            if (carMake.ToLower() == "porsche" && carModel.ToLower() == "911 carrera")
+            {
+                quote = quote + 25m;
+            }
+
+            for (int i = 0; i < speedingTickets; i++)
+            {
+                quote = quote + 10m;
+            }
+
+            if (radioDui == "yes")
+            {
+                quote = quote + (50m * .25m);
+            }
+
+            if (radioCoverage == "full")
+            {
+                quote = quote + (50m * .5m);
+            }
+
+            using (CarEntities db = new CarEntities())
+            {
+                var qt = new Quote();
+                qt.FirstName = firstName;
+                qt.LastName = lastName;
+                qt.EmailAddress = emailAddress;
+                qt.BirthDate = dateOfBirth;
+                qt.InsuranceQuote = quote;
+
+                db.Quotes.Add(qt);
+                db.SaveChanges();
+
+                var qts = db.Quotes.ToList();
+                return View(qts);
+            }
+                
+            
+       }
+    }
+}
