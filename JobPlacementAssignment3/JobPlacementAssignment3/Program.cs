a48e5ef133ad02c273fe7c232f7e9a714b803659
Howard Forbes <howard.d.forbes@outlook.com>
2019-08-27 13:37:14 -0700

    Added JobPlacementAssignment3

---
+++ JobPlacementAssignment3/JobPlacementAssignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPlacementAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Howard";
+            char[] charArray = name.ToCharArray();
+            char[] charReverse = new char[6];
+
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                charReverse[charArray.Length - (i+1)] = charArray[i];
+            }
+
+            string output = new string(charReverse);
+            Console.WriteLine(output);
+            
+            Console.ReadLine();
+        }
+    }
+}
