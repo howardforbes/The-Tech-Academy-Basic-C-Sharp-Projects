c4a864093d1ce45e22a8b188577f28078072b80c
Howard Forbes <howard.d.forbes@outlook.com>
2019-08-27 16:37:36 -0700

    Added JobPlacementAssignment5

---
+++ JobPlacementAssigment5/JobPlacementAssigment5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPlacementAssigment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder theResult = new StringBuilder();
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    theResult.Append("fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    theResult.Append("buzz");
+                }
+                else
+                {
+                    theResult.Append(i.ToString());
+                }
+            }
+
+            Console.WriteLine(theResult.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
