b137cab4d951409c27fbc91a6d7304b68b7eef3d
Howard Forbes <howard.d.forbes@outlook.com>
2019-08-27 12:16:51 -0700

    Added JobPlacementAssignment2

---
+++ JobPlacementAssignment2/JobPlacementAssignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPlacementAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numArray = new long[] { 900000000, 900000000, 90000000, 900000000, 900000000, 400000000, 600000000, 900000000 };
+            long sum = 0;
+
+            try
+            {
+                for (int i = 0; i < numArray.Length; i++)
+                {
+                    sum += numArray[i];
+                }
+                Console.WriteLine("The sum of the numbers is:" + sum);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
