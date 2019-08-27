f1be5751703874321daa649907bd882eba7dfeb1
Howard Forbes <howard.d.forbes@outlook.com>
2019-08-27 15:55:38 -0700

    Added JobPlacementAssignment4

---
+++ JobPlacementAssignment4/JobPlacementAssignment4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPlacementAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Howarddd";
+            char[] charArray = word.ToCharArray();
+            char[] charRemoveLetters = new char[8];
+
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                if(i == 0)
+                {
+                    charRemoveLetters[i] = charArray[i];
+                }
+                else
+                {
+                    if (!(charArray[i] == charArray[i - 1]))
+                    {
+                        charRemoveLetters[i] = charArray[i];
+                    }
+                }
+            }
+
+            string output = new string(charRemoveLetters);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
