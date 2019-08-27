c1c016e87fcb341e137ccc3c446499ac76659fe4
Howard Forbes <howard.d.forbes@outlook.com>
2019-08-27 11:22:38 -0700

    Added JobPlacementAssignment1

---
+++ JobPlacementAssignment1/JobPlacementAssignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPlacementAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[] { 2, 4, 6, 3, 7, 9 };
+            int sum = 0; 
+
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                if(numArray[i] % 2 == 1)
+                {
+                    sum += numArray[i];
+                }
+            }
+
+            Console.WriteLine("The sum of the odd numbers is: " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
